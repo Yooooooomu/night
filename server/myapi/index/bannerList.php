f9a32e307513a205cb44c2a90fe1b3b2cf1818c2
Yooooooomu <49017287+Yooooooomu@users.noreply.github.com>
2020-09-08 15:36:42 +0800

    清除缓存

--- server/myapi/index/bannerList.php
+++
@@ -1,9 +0,0 @@
-<?php
-  $index_banner = [
-	["img" => "https://bi09fclg.qcloud.la/img/index-swiper1.png", "href" => "1"],
-	["img" => "https://bi09fclg.qcloud.la/img/index-swiper2.png", "href" => "2"],
-	["img" => "https://bi09fclg.qcloud.la/img/index-swiper1.png", "href" => "3"],
-	["img" => "https://bi09fclg.qcloud.la/img/index-swiper2.png", "href" => "4"]
-  ];
-  echo json_encode($index_banner);
-?>
